901ee9e712e7f933554c9b2c33517d2c0a33c07e
Kibet <kibetdavidro@gmail.com>
2023-03-13 08:41:13 +0300

    update the database credentials

--- wp-config.php
+++ wp-config.php
@@ -23,13 +23,13 @@
 define( 'DB_NAME', 'contact-plugin' );
 
 /** Database username */
-define( 'DB_USER', 'kibet' );
+define( 'DB_USER', '' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'Kibetrono1996@' );
+define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', '' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
